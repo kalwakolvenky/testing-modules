be9ba1d004c462614ed6ffc04bef02b50815f944
kalwakolvenky <61227310+kalwakolvenky@users.noreply.github.com>
2022-03-06 12:35:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,11 @@ provider "aws" {
      
    region = "us-east-1"
 }
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
 
+    tags  = {
+        Name = "${var.vpc_name}"
+    }
+
+}
